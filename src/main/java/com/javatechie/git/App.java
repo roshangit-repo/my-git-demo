defb0e1d81722c794e91b95bb64aec83b9274425
roshangit-repo <dibyapatra48@gmail.com>
2020-01-11 12:35:19 +0530

    added my-git-demo project to local repo

---
+++ src/main/java/com/javatechie/git/App.java
@@ -0,0 +1,20 @@
+package com.javatechie.git;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public void addNumbers(int sum) {
+		System.out.println("Sum"+sum);
+		/*
+		 * if(sum==10) { System.out.println("sum is "+10); }
+		 */
+	}
+	
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
